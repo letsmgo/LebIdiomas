bc8aaf4c38c3be756b087e48be708061ed0634ca
Bárbara Romero <bqromero@gmail.com>
2021-09-30 13:21:40 -0300

    Criar método toString para a subclasse Espanhol

--- src/br/com/zup/Espanhol.java
+++ src/br/com/zup/Espanhol.java
@@ -3,16 +3,22 @@ package br.com.zup;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Espanhol extends Curso{
+public class Espanhol extends Curso {
   List<Espanhol> espanholList = new ArrayList<>();
 
   public Espanhol(String nomeDoCurso, double valor, String duracao, int qtdModulos, String professor) {
     super(nomeDoCurso, valor, duracao, qtdModulos, professor);
   }
 
-  public void adicionarAlunoEspanhol (Espanhol novoAlunoEspanhol){
+  public void adicionarAlunoEspanhol(Espanhol novoAlunoEspanhol) {
     espanholList.add(novoAlunoEspanhol);
   }
 
-
+  @Override
+  public String toString() {
+    StringBuilder mostrarDados = new StringBuilder();
+    mostrarDados.append(super.toString());
+    mostrarDados.append("Lista de alunos: " + espanholList);
+    return mostrarDados.toString();
+  }
 }

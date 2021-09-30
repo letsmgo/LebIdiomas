14b0be3bac08bd525f3d91a9bc9381d9d35ccbae
Elaine <elaine.silva@zup.com.br>
2021-09-30 15:09:39 -0300

    criar sistema e metodo exibir catalogo

---
+++ src/br/com/zup/Sistema.java
@@ -0,0 +1,22 @@
+package br.com.zup;
+
+public class Sistema {
+
+    public static void exibirCatalogo() {
+        System.out.println();
+        System.out.println("-----------------------------------------------");
+        System.out.println("|            Bem-vinde a LEB SCHOOL            |");
+        System.out.println("|           Nossa escola de idiomas!           |");
+        System.out.println("|                                              |");
+        System.out.println("|      Aqui temos cursos de três idiomas:      |");
+        System.out.println("|       Inglês  |  Espanhol  |  Francês        |");
+        System.out.println("|                                              |");
+        System.out.println("|        Caso se matricule até o dia 20,       |");
+        System.out.println("|   você ganha DESCONTO DE 25% do valor total  |");
+        System.out.println("|         para todas as mensalidades!          |");
+        System.out.println("|                                              |");
+        System.out.println("|          -desconto não acumulativo-          |");
+        System.out.println("------------------------------------------------");
+    }
+
+}
